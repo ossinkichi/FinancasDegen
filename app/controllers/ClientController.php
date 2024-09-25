388da55ab43cbedb9bdd4eda8fdad1189109eeb5
Carlos junior <95056634+ossinkichi@users.noreply.github.com>
2024-09-25 22:05:13 +0000

    enviando

---
+++ app/controllers/ClientController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace app\controllers;
+
+class ClientController{
+
+  private $db;
+
+  public function getClient(){}
+
+  public function getAllClients(){}
+  
+  public function setNewClient(){}
+}
---
+++ app/controllers/UserController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace app\controllers;
+
+class UserController {
+  private $db;
+
+  public function getUser($data){}
+
+  public function setNewUser($data){}
+}
