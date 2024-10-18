8ecf8846ba7ca7e766e6081d91e3bb6add3cca73
HianSilvaDev <hianvitorsantossilva@gmail.com>
2024-10-18 14:09:26 +0000

    create the header an footer for reuse

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,11 @@
 import "./App.css";
-
+import Header from ".//Components/Header";
+import Footer from ".//Components/Footer";
 function App() {
+  
   return <div className="App">
-    <h1>Hi</h1>
+    <Header />
+    <Footer />
   </div>;
 }
 
---
+++ frontend/src/Components/Header.tsx
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+import "./css/Header.css";
+
+const Header: React.FC = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  return (
+    <header className="header">
+      <div className="container">
+         <div className="logo">BlueBalance</div>
+
+      <nav className={`nav ${isMenuOpen ? "open" : ""}`}>
+        <ul>
+          <li>
+            <a href="#home">Inicio</a>
+          </li>
+          <li>
+            <a href="#services">Planos</a>
+          </li>
+          <li>
+            <a href="#about">Sobre</a>
+          </li>
+          <li>
+            <a href="#contact">Suporte</a>
+          </li>
+        </ul>
+      </nav>
+      </div>
+
+
+      <div className="container">
+        <div className="button">
+          <i className="bi bi-person"></i>
+        </div>
+
+        <div className="menu-icon button" onClick={toggleMenu}>
+          <i className="bi bi-list"></i>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
