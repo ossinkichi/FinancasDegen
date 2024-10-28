89fde9ec8fe7acbf92552e19a96d35d29e4e679e
Hian Silva <hianvitorsantossilva@gmail.com>
2024-10-28 12:05:01 +0000

    add associated requirement to access the dashboard

--- frontend/src/Components/template/PrivateRoutes.tsx
+++ frontend/src/Components/template/PrivateRoutes.tsx
@@ -3,9 +3,10 @@ import { Outlet, Navigate } from 'react-router-dom';
 import { useAuth } from './AuthContext';
 
 const PrivateRoute: React.FC = () => {
-  const { isAuthenticated } = useAuth();
+  const { isAuthenticated, isAssociated } = useAuth();
+  if(!isAssociated) return <Navigate to="/company" />
 
-  return isAuthenticated ? <Outlet /> : <Navigate to="/auth"/>
+  return isAuthenticated ? <Outlet /> : <Navigate to="/auth" />
 };
 
 export default PrivateRoute;
--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -1,9 +1,10 @@
 import React from "react";
+import "../Components/css/Dashboard.css";
 
 const Dashboard = () => {
     return (
         <>
-        
+            <h1>Dashboard</h1>
         </>
     )
 }
